384736bdddc0ecee3d7a77953ea5b47f7adc8b6e
roccho <40359643+roccho-dev@users.noreply.github.com>
2026-06-24 04:43:36 +0900

    governance: add feat consumer flake example

---
+++ example/feat-consumer/flake.nix
@@ -0,0 +1,23 @@
+{
+  description = "minimal feat consumer of governance repo-governance package";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    adrsRecords = {
+      url = "github:roccho-dev/adrs/main";
+      flake = false;
+    };
+    governance.url = "github:roccho-dev/governance";
+    governance.inputs.nixpkgs.follows = "nixpkgs";
+    governance.inputs.adrsRecords.follows = "adrsRecords";
+  };
+
+  outputs = { nixpkgs, governance, ... }:
+    {
+      checks = governance.lib.mkRepoGovernanceChecks {
+        inherit nixpkgs;
+        systems = [ "x86_64-linux" ];
+        repoSnapshot = ./governance/repo-governance.json;
+      };
+    };
+}
